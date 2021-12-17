6fa9fba7c04ffaf09de5ffc1e055643c41ee2981
jongchan988 <jongchan988@gmail.com>
2021-12-17 20:49:03 +0900

    feat: pushNamed 페이지 추가 후 이동 시 argument 값 전달 적용

--- lib/sub_detail.dart
+++ lib/sub_detail.dart
@@ -6,21 +6,47 @@ class SubDetail extends StatefulWidget{
 }
 
 class _SubDetail extends State<StatefulWidget> {
+  List<String> todoList = new List.empty(growable: true);
+
+  @override
+  void initState(){
+    super.initState();
+    todoList.add('당근 사오기');
+    todoList.add('약 사오기');
+    todoList.add('청소하기');
+    todoList.add('부모님께 전화하기');
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Sub Detail Example'),
       ),
-      body: Container(
-        child: Center(
-          child: ElevatedButton(
-            child: Text('두 번째 페이지로 이동하기'),
-            onPressed: (){
-              Navigator.of(context).pushNamed('/second');
-            },
-          ),
-        ),
+      body: ListView.builder(
+        itemBuilder: (context, index){
+          return Card(
+            child: InkWell(
+              child: Text(
+                todoList[index],
+                style: TextStyle(
+                  fontSize: 30
+                ),
+              ),
+              onTap: (){
+                Navigator.of(context).pushNamed(
+                  '/third',
+                  arguments: todoList[index]
+                );
+              },
+            ),
+          );
+        },
+        itemCount: todoList.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
       ),
     );
   }
