e75fa982199d84ab6bb7fa6f880524bac35faf1a
jongchan988 <jongchan988@gmail.com>
2021-12-17 19:15:16 +0900

    feat: Navigator 사용 및 routes 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,73 @@
 import 'package:flutter/material.dart';
-import 'largeFileMain.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SubPage Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+       '/': (context) => FirstPage(),
+       '/second': (context) => SecondPage()
+      },
+
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage>{
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      home: LargeFileMain(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub Page Main'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('첫 번째 페이지'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).pushNamed('/second');
+        },
+        child: Icon(
+          Icons.add
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+            child: Text('돌아가기'),
+          ),
+        ),
+      ),
     );
   }
 }
