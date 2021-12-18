177ed039915f54436e0aba700ddc930c7aa083de
jongchan988 <jongchan988@gmail.com>
2021-12-18 19:38:15 +0900

    feat: repo, appDocuments txt파일 동기화 및 해당 데이터로 리스트 뷰 구현

--- lib/file_app.dart
+++ lib/file_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FileApp extends StatefulWidget {
   @override
@@ -9,11 +10,52 @@ class FileApp extends StatefulWidget {
 
 class _FileApp extends State<FileApp> {
   int _count = 0;
+  List<String> itemList = new List.empty(growable: true);
+  TextEditingController controller = new TextEditingController();
 
+  Future<List<String>> readListFile() async {
+    List<String> itemList = new List.empty(growable: true);
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? firstCheck = pref.getBool(key);
+    var dir = await getApplicationDocumentsDirectory();
+    bool fileExist = await File(dir.path + '/fruit.txt').exists();
+
+    if (notExistData(firstCheck, fileExist)){
+      pref.setBool(key, true);
+      var file = await DefaultAssetBundle.of(context).loadString('repo/fruit.txt');
+      File(dir.path + '/fruit.txt').writeAsStringSync(file);
+      return parseList(file, itemList);
+    }
+    return await getDataFromAppDir(itemList, dir);
+  }
+
+
+  Future <List<String>> getDataFromAppDir(itemList, dir) async{
+    var file = await File(dir.path + '/fruit.txt').readAsString();
+    return parseList(file, itemList);
+  }
+
+  List<String> parseList(String file, List<String> itemList){
+    var array = file.split('\n');
+    for (var item in array){
+      itemList.add(item);
+    }
+    return itemList;
+  }
+
+  bool notExistData(bool? firstCheck, bool fileExist) => firstCheck == null || firstCheck == false || fileExist == false;
   @override
   void initState(){
     super.initState();
     readCountFile();
+    initData();
+  }
+  void initData() async{
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
   }
   @override
   Widget build(BuildContext context){
@@ -23,18 +65,31 @@ class _FileApp extends State<FileApp> {
       ),
       body: Container(
         child: Center(
-          child: Text(
-            '$_count',
-            style: TextStyle(fontSize: 40),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index){
+                    return Card(
+                      child: Text(
+                        itemList[index],
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    );
+                  },
+                  itemCount: itemList.length,
+                )
+              )
+            ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          setState(() {
-            _count++;
-          });
-          writeCountFile(_count);
         },
         child: Icon(Icons.add),
       ),
