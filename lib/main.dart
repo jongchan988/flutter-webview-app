8977047d9f96c863e9e9d92ac19826b7f9e2e317
jongchan988 <jongchan988@gmail.com>
2021-12-19 21:03:15 +0900

    feat: 페이지 이동 에니메이션 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'entity/people.dart';
+import 'second_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -139,6 +140,27 @@ class _AnimationApp extends State<AnimationApp>{
                   });
                 },
                 child: Text('${_opacity == 1 ? '사라지기': '나타나기'}'),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => SecondPage()
+                    )
+                  );
+                },
+                child: SizedBox(
+                  width: 200,
+                  child: Row(
+                    children: <Widget>[
+                      Hero(
+                        tag: 'detail',
+                        child: Icon(Icons.cake),
+                      ),
+                      Text('이동하기')
+                    ],
+                  ),
+                ),
               ),
             ],
             mainAxisAlignment: MainAxisAlignment.center,
