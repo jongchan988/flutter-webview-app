f0192de7289c8f1d3c9f7dd196c0721506e35969
jongchan988 <jongchan988@gmail.com>
2021-12-29 21:30:14 +0900

    feat: 전면 광고 추가

--- lib/memo_add.dart
+++ lib/memo_add.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'memo.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class MemoAddPage extends StatefulWidget {
   final DatabaseReference reference;
@@ -12,12 +13,52 @@ class MemoAddPage extends StatefulWidget {
 class _MemoAddPage extends State<MemoAddPage> {
   TextEditingController? titleController;
   TextEditingController? contentController;
+  InterstitialAd? _interstitialAd;
+
+  void _createInterstitialAd(){
+    InterstitialAd.load(
+      adUnitId: InterstitialAd.testAdUnitId,
+      request: AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (InterstitialAd ad){
+          print('$ad loaded');
+          _interstitialAd = ad;
+        },
+        onAdFailedToLoad: (LoadAdError error){
+          print('InterstitialAd failed to load: $error');
+          _interstitialAd = null;
+        }
+      )
+    );
+  }
+
+  void _showInterstitialAd(){
+    if(_interstitialAd == null){
+      return;
+    }
+    _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (InterstitialAd ad) => print('ad onAdShowedFullScreenContent'),
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        print('$ad onAdDismissedFullScreenContent');
+        ad.dispose();
+        _createInterstitialAd();
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error){
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+        _createInterstitialAd();
+      },
+    );
+    _interstitialAd!.show();
+    _interstitialAd = null;
+  }
 
   @override
   void initState(){
     super.initState();
     titleController = TextEditingController();
     contentController = TextEditingController();
+    _createInterstitialAd();
   }
 
   @override
@@ -57,6 +98,7 @@ class _MemoAddPage extends State<MemoAddPage> {
                   ).then((_){
                     Navigator.of(context).pop();
                   });
+                  _showInterstitialAd();
                 },
                 child: Text('저장하기'),
                 shape: OutlineInputBorder(
